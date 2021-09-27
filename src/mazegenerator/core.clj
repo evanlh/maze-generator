295c2288dc02a763482784edd1a5aaf36ce5ef08
Evan Lawrence-Hurt <evanlh@gmail.com>
2021-09-26 21:40:52 -0400

    save state... making progress on my other recursive solution, alternating vertical and horizontal divisions

--- src/mazegenerator/core.clj
+++ src/mazegenerator/core.clj
@@ -123,6 +123,155 @@
   ([lx rx ty dy delta]
    [(+ lx delta) (- rx delta) (+ ty delta) (- dy delta)]))
 
+(defn render-room-to-maze
+  "Given the `maze` vector, set to `wall-value` the boundary of the rect specified
+   by `:top-left` and `:bottom-right` in `room` and to `door-value` the `:doors`
+   specified in `room`. Note that the `:top-left` coordinate is inside the room and
+   the `:bottom-right` coordinate is a part of the wall (still questioning this choice).
+   The whole wall region will be overwritten so it's necessary to specify all `:doors`"
+  ([maze room] (render-room-to-maze maze room 1 0))
+  ([maze room wall-value door-value]
+   (let [{top-left :top-left
+          bottom-right :bottom-right
+          doors :doors} room
+         [top-left-x top-left-y] top-left
+         [bottom-right-x bottom-right-y] bottom-right
+         x1 (dec top-left-x) ;; the coords for room are the inner part of the room
+         x2 bottom-right-x
+         y1 (dec top-left-y)
+         y2 bottom-right-y
+         top-line (hline y1 x1 x2)
+         bottom-line (hline y2 x1 x2)
+         left-line (vline x1 y1 y2)
+         right-line (vline x2 y1 (inc y2))
+         all-lines (concat top-line bottom-line right-line left-line)
+         maze-with-border (maze-assoc-at-seq maze all-lines wall-value)
+         maze-with-border-and-doors (maze-assoc-at-seq maze-with-border doors door-value)]
+     maze-with-border-and-doors)))
+
+(defn room-height [room] (- (second (room :bottom-right)) (second (room :top-left))))
+(defn room-width [room] (- (first (room :bottom-right)) (first (room :top-left))))
+
+(defn door-borders-room? [door top-left bottom-right]
+  (let [[x y] door
+        [top-left-x top-left-y] top-left
+        [bottom-right-x bottom-right-y] bottom-right]
+    (or (and (= x (dec top-left-x))
+             (>= y top-left-y)
+             (< y bottom-right-y))
+        (and (= x bottom-right-x)
+             (>= y top-left-y)
+             (< y bottom-right-y))
+        (and (= y (dec top-left-y))
+             (>= x top-left-x)
+             (< x bottom-right-x))
+        (and (= y bottom-right-y)
+             (>= x top-left-x)
+             (< x bottom-right-x)))))
+
+(defn is-valid-room? [room]
+  "A valid room has `:top-left` strictly < `:bottom-right` and doors bounding this region"
+  (let [[top-left-x top-left-y] (room :top-left)
+        [bottom-right-x bottom-right-y] (room :bottom-right)
+        doors (room :doors)]
+    (and (< top-left-x bottom-right-x)
+         (< top-left-y bottom-right-y)
+         (> top-left-x 0)
+         (> top-left-y 0)
+         (every? #(door-borders-room? % (:top-left room) (:bottom-right room)) doors))))
+
+(defn maze-generation-recursive-2
+  ([size] (maze-generation-recursive-2 size (* size size)))
+  ([size max-iterations]
+   (let [maze (maze-init size)
+         dec-size (dec size)
+         ;; simplifying somewhat by only having entry/exit top/bottom
+         initial-doors [[(rand-between 1 dec-size) 0] [(rand-between 1 dec-size) dec-size]]
+         initial-room {:top-left [1, 1], :bottom-right [dec-size dec-size], :doors initial-doors }]
+     (println initial-room)
+     (recursively-build-room-list initial-room []))))
+
+;; the task is to
+;; 1. create up to 4 subrooms given 1 larger room
+;;     and a list of already existing doors
+;; 2. the divider cannot block any of hte doors
+;; 3. horizontal/vertical division stops when the width/height of the resulting
+;;    inner room is <= 2 (?)
+
+;; we will approach this problem by:
+;; - adding a horizontal divider if the height is >= the width
+;;   - divider position is random, excluding surrounding door coordinates
+;; - adding a vertical divider otherwise
+;; - pasing thru a list of all doors outside the current room
+;; - filtering that list to only the doors bounding the current room
+
+(defn recursively-build-room-list [room room-list]
+  (let [doors (:doors room)
+        [top-left-x top-left-y] (:top-left room)
+        [bottom-right-x bottom-right-y] (:bottom-right room)]
+    (if (not (is-valid-room? room))
+      room-list
+      (if (>= (room-height room) (room-width room))
+        (let [avoid-y-coords (distinct (map second doors))]
+          (if (<= (room-height room) (count avoid-y-coords))
+            room-list
+            ;; divide horizontal & recurse
+            (divide-horizontal room room-list avoid-y-coords)))
+
+        (let [avoid-x-coords (distinct (map second doors))]
+          (if (<= (room-width room) (count avoid-x-coords))
+            room-list
+            (divide-vertical room room-list avoid-x-coords)))))))
+
+(defn divide-horizontal [room room-list avoid-y-coords]
+  (let [doors (:doors room)
+        [top-left-x top-left-y] (:top-left room)
+        [bottom-right-x bottom-right-y] (:bottom-right room)
+        ;; pick a y coordinate for the divider, excluding the doors
+        divider-y (rand-between-except top-left-y bottom-right-y avoid-y-coords)
+        ;; pick a random x coordinate on that divider line as a door
+        divider-door [(rand-between top-left-x bottom-right-x) divider-y]
+        ;; define the rooms on each side of the divider line
+        new-doors (conj doors divider-door)
+        top-left-top (:top-left room)
+        bottom-right-top  [bottom-right-x divider-y]
+        top-room {:top-left top-left-top
+                  :bottom-right bottom-right-top
+                  :doors (filter #(door-borders-room? % top-left-top bottom-right-top) new-doors)}
+        top-left-bottom [top-left-x (inc divider-y)]
+        bottom-right-bottom (:bottom-right room)
+        bottom-room {:top-left top-left-bottom
+                     :bottom-right bottom-right-bottom
+                     :doors (filter #(door-borders-room? % top-left-bottom bottom-right-bottom) new-doors)}]
+    (concat room-list
+            (cons top-room (recursively-build-room-list top-room '()))
+            (cons bottom-room (recursively-build-room-list bottom-room '())))))
+
+(defn divide-vertical [room room-list avoid-x-coords]
+  (let [doors (:doors room)
+        [top-left-x top-left-y] (:top-left room)
+        [bottom-right-x bottom-right-y] (:bottom-right room)
+        ;; pick an x coordinate for the divider, excluding the doors
+        divider-x (rand-between-except top-left-x bottom-right-x avoid-x-coords)
+        ;; pick a random y coordinate on that divider line as a door
+        divider-door [divider-x (rand-between top-left-y bottom-right-y)]
+        ;; define the rooms on each side of the divider line
+        new-doors (conj doors divider-door)
+        top-left-left (:top-left room)
+        bottom-right-left  [divider-x bottom-right-y]
+        left-room {:top-left top-left-left
+                   :bottom-right bottom-right-left
+                   :doors (filter #(door-borders-room? % top-left-left bottom-right-left) new-doors)}
+        top-left-right [(inc divider-x) top-left-y]
+        bottom-right-right (:bottom-right room)
+        right-room {:top-left top-left-right
+                    :bottom-right bottom-right-right
+                    :doors (filter #(door-borders-room? % top-left-right bottom-right-right) new-doors)}]
+    (concat room-list
+            (cons left-room (recursively-build-room-list left-room '()))
+            (cons right-room (recursively-build-room-list right-room '())))))
+
+
 (defn maze-generation-recursive-algorithm
   ([size] (maze-generation-recursive-algorithm size (* size size)))
   ([size max-iterations]
@@ -196,3 +345,15 @@
 ;; - You're missing basic math ops like translating a Point by a vector, scaling a Rectangle, etc
 ;;   that might be useful here.
 ;;   - In part this seems to be a failure to recognize these core types and write utility funcs for them
+;;      .... OTOH maybe these aren't hte important types? Point yes, Rect less clear
+
+
+;; All of the icns/decs seem like a red flags
+;; Got fixated on the grid-based model I started with, didn't build higher order data types
+;; Feedback from maze-print and draw are helpful at seeing what is wrong, course correcting
+;; the details but not so much the big picture
+
+
+;; some kind of path-dependence inside the REPl that going to pen & paper breaks
+
+;; Don't like how the initial condition seems so much different from the inner recursion
